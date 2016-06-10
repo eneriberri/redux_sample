0da2d5f3f648b7f58e93f34680f9d0947a21f27f
irene <irene@chegg.com>
2016-06-10 13:25:21 -0700

    implement initial state

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -49,4 +49,12 @@ describe('reducer', () => {
     }));
   });
 
+  it('has an initial state', () => {
+    const action = {type: 'SET_ENTRIES', entries: fromJS(['Princess Mononoke'])};
+    const nextState = reducer(undefined, action);
+    expect(nextState).to.equal(fromJS({
+      entries: ['Princess Mononoke']
+    }));
+  });
+
 });
